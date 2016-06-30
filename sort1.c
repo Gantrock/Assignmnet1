fca8f3dd89c675bd9e27d2ae698eb93eead6d2cf
Gantrock <Gantrock@users.noreply.github.com>
2016-06-30 16:13:41 -0700

    Update sort1.c
    Added code for Bubblesort

--- sort1.c
+++ sort1.c
@@ -1,1 +1,21 @@
-
+#include "bubblesort.h"
+void bubble_sort(int list*, int n)
+{
+  int c, d, t;
+ 
+  for (c = 0 ; c < ( n - 1 ); c++)
+  {
+    for (d = 0 ; d < n - c - 1; d++)
+    {
+      if (list[d] > list[d+1])
+      {
+        /* Swapping */
+ 
+        t         = list[d];
+        list[d]   = list[d+1];
+        list[d+1] = t;
+      }
+    }
+  }
+}
+}
